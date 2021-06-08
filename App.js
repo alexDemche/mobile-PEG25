4b5733c7e4acf2f23f0389038d06c1b436fa9828
Oleksii Demchenko <o.demchenko@wrenchtech.io>
2021-06-08 21:00:59 +0300

    added structure

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      < View style={styles.container}>
+        <Text>Open up App.js to start working on your app!!</Text>
+        <StatusBar style="auto"/>
+      </View>
+    </NavigationContainer>
   );
 }
 
